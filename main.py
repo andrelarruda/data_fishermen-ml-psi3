8e2ace63238781d8395d1e1519ac15f9c1f50c90
Hugo Teixeira <hugo.teixeira@dreamm.com.br>
2022-07-12 22:14:01 -0300

    Add new plots of age and residence type

--- main.py
+++ main.py
@@ -16,24 +16,27 @@ def load_data(nrows):
 
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-df = load_data(10000)
+data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Data loaded')
 
 st.subheader('Raw data')
-st.write(df)
+st.write(data)
 
 #st.subheader('Quantity of null data by column')
 #st.text(df.isnull().sum())
 
-x = df[['age', 'hypertension', 'gender']].copy()
+x = data[['age', 'hypertension', 'gender']].copy()
 
-#st.table(df['age'].loc[df['stroke']==1])
-had_stroke = df['age'].loc[df['stroke']==1]
-no_stroke = df['age'].loc[df['stroke']==0]
-
-
-st.line_chart(data=had_stroke, width=0, height=0, use_container_width=True)
-
+df_stroke=data.loc[data["stroke"]==1]
+df_no_stroke=data.loc[data["stroke"]==1]
 
 st.subheader('Age')
+
+df_stroke_age = df_stroke.groupby("age")["stroke"].count()
+st.area_chart(data=df_stroke_age)
+
+st.subheader('Residence Type')
+
+df_stroke_glucose = df_stroke.groupby("residence_type")["stroke"].count()
+st.bar_chart(data=df_stroke_glucose)
