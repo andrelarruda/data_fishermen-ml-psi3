9d05343e90adff7a77dd7461b68d6eb6b1136047
Andre Arruda <andre.arruda@fastsolucoes.com.br>
2022-07-12 21:58:22 -0300

    Grafico de linhas para mostrar a idade das pessoas que tiveram AVC e nao tiveram. Melhoria: Linkar esses 2 graficos

--- main.py
+++ main.py
@@ -16,11 +16,24 @@ def load_data(nrows):
 
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
+df = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Data loaded')
 
 st.subheader('Raw data')
-st.write(data)
+st.write(df)
+
+#st.subheader('Quantity of null data by column')
+#st.text(df.isnull().sum())
+
+x = df[['age', 'hypertension', 'gender']].copy()
+
+#st.table(df['age'].loc[df['stroke']==1])
+had_stroke = df['age'].loc[df['stroke']==1]
+no_stroke = df['age'].loc[df['stroke']==0]
+
+
+st.line_chart(data=had_stroke, width=0, height=0, use_container_width=True)
+
 
 st.subheader('Age')
