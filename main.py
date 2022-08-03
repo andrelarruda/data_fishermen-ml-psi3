921707f13412ba6d7921aee20e51d755ba72c3cf
Andre Arruda <andre.arruda@fastsolucoes.com.br>
2022-08-02 23:40:57 -0300

    Adicionando titulo e favicon a pagina

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(page_title="Stroke Analysis", page_icon=":broken_heart:")
+
 st.title('Strokes')
 
 AGE_COLUMN = 'age'
